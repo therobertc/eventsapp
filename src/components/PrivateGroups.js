15fd9e4ac7674ae894511f2be38038042bbc4a97
Rob J. Calderon <rob.jcalderon@gmail.com>
2021-06-10 12:05:06 -0700

    chat tabs

--- src/components/PrivateGroups.js
+++ src/components/PrivateGroups.js
@@ -17,13 +17,12 @@ import { Icon, Header, Left, Right, Body, Button } from "native-base";
 import Messages from "../components/Messages";
 import TrendingStocks from "../components/TrendingStocks";
 import { Feather } from "@expo/vector-icons";
-import AddGroup from "./Sub/AddGroup";
 import fire, { firestore } from "../database/firebase";
 import StockGroupCard from "../components/StockGroupCard";
 import ChartComp from "../components/ChartComp";
 import ChatTabs from "../components/Tabs/ChatTabs";
 
-const Chat = (props) => {
+const Private = (props) => {
   const isVisible = useIsFocused();
   const [groups, setGroups] = useState([]);
   const [publicgroups, setpublicgroups] = useState([]);
@@ -129,197 +128,66 @@ const Chat = (props) => {
       setRefreshing(false);
     }, 3000);
   }
-  const onShare = async () => {
-    try {
-      const result = await Share.share({
-        message:
-          "Hey - I have an invite to StockChat and want you to join. Here is the link! https://stockchatapp.com",
-      });
-      if (result.action === Share.sharedAction) {
-        if (result.activityType) {
-          // shared with activity type of result.activityType
-        } else {
-          // shared
-        }
-      } else if (result.action === Share.dismissedAction) {
-        // dismissed
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  };
 
   return (
-    <View style={styles.container}>
-      <Header
-        style={{
-          backgroundColor: "#282c34",
-          borderBottomWidth: 0.2,
-          borderBottomColor: "#282c34",
-        }}
-      >
-        <Left>
-          <Text style={styles.logotext}>#stockchat</Text>
-          {/* <Feather
-            style={{
-              color: "#FFF",
-              fontWeight: "bold",
-              paddingHorizontal: Platform.OS === "ios" ? 10 : 10,
-              fontSize: 30,
-            }}
-            name="menu"
-            onPress={onShare}
-          /> */}
-        </Left>
-        <Right>
-          <Feather
-            style={{
-              color: "#FFF",
-              fontWeight: "bold",
-              paddingHorizontal: Platform.OS === "ios" ? 10 : 10,
-              fontSize: 30,
-            }}
-            name="user-plus"
-            onPress={onShare}
-          />
+    <ScrollView
+      style={styles.container}
+      refreshControl={
+        <RefreshControl
+          tintColor="#fff"
+          titleColor="#fff"
+          colors={["#fff"]}
+          refreshing={refreshing}
+          onRefresh={refreshChats}
+        />
+      }
+    >
+      {/* <TrendingStocks {...props} /> */}
 
-          <Feather
-            style={{
-              color: "#FFF",
-              fontWeight: "bold",
-              paddingHorizontal: Platform.OS === "ios" ? 10 : 10,
-              fontSize: 30,
-            }}
-            name="bell"
-            onPress={() => props.navigation.navigate("Activity")}
-          />
-        </Right>
-      </Header>
+      <StockGroupCard
+        ticker="$COMINGSOON"
+        //pctchange="+3.521%"
+        // onPress={() => {
+        //   props.navigation.navigate("StockChat", {
+        //     itemName: "$NET",
+        //     // itemId: item.id,
+        //     // itemName: item.login,
+        //     itemPic: "https://i.stack.imgur.com/l60Hf.png"
+        //   });
+        // }}
+        msg="Private groups coming soon"
+      />
 
-      <ScrollView
-        style={styles.col2}
-        refreshControl={
-          <RefreshControl
-            tintColor="#fff"
-            titleColor="#fff"
-            colors={["#fff"]}
-            refreshing={refreshing}
-            onRefresh={refreshChats}
-          />
-        }
-      >
-        <View style={styles.headercard}>
-          <Text
-            style={{
-              fontSize: 14,
-              color: "#FFF",
-              fontWeight: "600",
-              marginBottom: 10,
-              marginHorizontal: 20,
-            }}
-          >
-            ACTIVITY
-          </Text>
-          <Text style={{ fontSize: 18, color: "#FFF", marginHorizontal: 20 }}>
-            Today's trending assets, scroll for more.
-          </Text>
-
-          <View
-            style={{
-              //flexDirection: "row",
-              //justifyContent: "space-between",
-              marginTop: 20,
-              start: 20,
-            }}
-          >
-            <TrendingStocks {...props} />
-          </View>
-
-          {/* <Button
-            full
-            iconLeft
-            primary
-            style={{
-              borderRadius: 10,
-              //marginTop: 20,
-              //marginHorizontal: 30,
-              height: 50,
-            }}
-            // onPress={() => this.props.navigation.navigate("Subscribe")}
-          >
-            <Text
-              style={{
-                flex: 2,
-                fontSize: 18,
-                color: "white",
-                fontWeight: "bold",
-                textAlign: "center",
+      {/* <View style={styles.card}> */}
+      {/* <FlatList
+        data={publicgroups}
+        keyExtractor={(item, index) => "key" + index}
+        renderItem={({ item }) => {
+          const name = item.groupName;
+          const totalmembers =
+            item && item.totalmembers ? item.totalmembers : 0;
+          const lastmessage = item && item.lastmessage ? item.lastmessage : "";
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                props.navigation.navigate("Discussion", {
+                  item,
+                  itemPic: "https://i.stack.imgur.com/l60Hf.png",
+                });
               }}
             >
-              Update Portfolio
-            </Text>
-          </Button> */}
-        </View>
+              <Messages
+                item={name}
+                totalmembers={totalmembers}
+                lastmessage={lastmessage}
+              ></Messages>
+            </TouchableOpacity>
+          );
+        }}
+      /> */}
+      {/* </View> */}
 
-        {/* <TrendingStocks {...props} /> */}
-
-        {/* <StockGroupCard
-            ticker="$NET"
-            pctchange="+3.521%"
-            onPress={() => {
-              props.navigation.navigate("StockChat", {
-                itemName: "$NET",
-                // itemId: item.id,
-                // itemName: item.login,
-                itemPic: "https://i.stack.imgur.com/l60Hf.png"
-              });
-            }}
-            msg="This stock is trending"
-          /> */}
-
-        <View style={styles.card}>
-          <Text
-            style={{
-              fontSize: 14,
-              color: "#FFF",
-              fontWeight: "600",
-              //marginBottom: 10,
-            }}
-          >
-            PUBLIC GROUPS
-          </Text>
-
-          {/* <ChatTabs></ChatTabs> */}
-          <FlatList
-            data={publicgroups}
-            keyExtractor={(item, index) => "key" + index}
-            renderItem={({ item }) => {
-              const name = item.groupName;
-              const totalmembers =
-                item && item.totalmembers ? item.totalmembers : 0;
-              const lastmessage =
-                item && item.lastmessage ? item.lastmessage : "";
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    props.navigation.navigate("Discussion", {
-                      item,
-                      itemPic: "https://i.stack.imgur.com/l60Hf.png",
-                    });
-                  }}
-                >
-                  <Messages
-                    item={name}
-                    totalmembers={totalmembers}
-                    lastmessage={lastmessage}
-                  ></Messages>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </View>
-
-        {/* <FlatList
+      {/* <FlatList
             data={groups}
             keyExtractor={(item, index) => "key" + index}
             renderItem={({ item }) => {
@@ -339,51 +207,11 @@ const Chat = (props) => {
               );
             }}
           /> */}
-        {/* </View> */}
-      </ScrollView>
-
-      {/* <TouchableOpacity
-        onPress={() => props.navigation.navigate("AddGroup")}
-        style={{
-          borderWidth: 1,
-          borderColor: "rgba(0,0,0,0.2)",
-          alignItems: "center",
-          justifyContent: "center",
-          width: 200,
-          position: "absolute",
-          bottom: 10,
-          right: 110,
-          height: 50,
-          backgroundColor: "#ed6ac3",
-          borderRadius: 100,
-          flexDirection: "row",
-        }}
-      >
-        <Feather
-          style={{
-            color: "#FFF",
-            fontWeight: "bold",
-            //paddingHorizontal: Platform.OS === "ios" ? 10 : 10,
-            fontSize: 20,
-          }}
-          name="plus"
-        />
-        <Text
-          style={{
-            textAlign: "center",
-            fontSize: 18,
-            fontWeight: "600",
-            color: "#FFF",
-            paddingLeft: 10,
-          }}
-        >
-          Start Group
-        </Text>
-      </TouchableOpacity> */}
-    </View>
+      {/* </View> */}
+    </ScrollView>
   );
 };
-export default Chat;
+export default Private;
 
 const styles = StyleSheet.create({
   list: {
@@ -421,7 +249,7 @@ const styles = StyleSheet.create({
     paddingTop: 30,
   },
   container: {
-    height: "100%",
+    //height: "100%",
     backgroundColor: "#282c34",
     // left: 0,
     // right: 0,
@@ -543,34 +371,16 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     shadowColor: "#000",
     //marginHorizontal: 10,
-    shadowOpacity: 0.5,
-    //marginVertical: 5,
-    elevation: 1,
-    //backgroundColor: "#e8eef1",
-    //backgroundColor: "#35383F"
-    backgroundColor: "#35383F",
-    //backgroundColor: "#282c34"
-    backgroundColor: "#282c34",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-  },
-  headercard: {
-    shadowOffset: { width: 0.5, height: 0.5 },
-    shadowRadius: 5,
-    shadowColor: "#000",
-    //marginHorizontal: 10,
     //shadowOpacity: 0.5,
-    marginVertical: 5,
+    //marginVertical: 5,
     //elevation: 1,
     //backgroundColor: "#e8eef1",
     //backgroundColor: "#35383F"
-    backgroundColor: "#35383F",
+    //backgroundColor: "#35383F",
     //backgroundColor: "#282c34"
     backgroundColor: "#282c34",
-    borderRadius: 20,
-    //paddingHorizontal: 20,
-    paddingVertical: 20,
+    //borderRadius: 20,
+    // paddingHorizontal: 20,
+    //paddingVertical: 20,
   },
 });
---
+++ src/components/Tabs/ChatTabs.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import WSBTrends from "../WSBTrends";
+import InsiderTrades from "../InsiderTrades";
+import Notifications from "../Notifications";
+import OptionsAlerts from "../OptionsAlerts";
+import PublicGroups from "../PublicGroups";
+import PrivateGroups from "../PrivateGroups";
+
+const Tab = createMaterialTopTabNavigator();
+
+function ChatTabs() {
+  return (
+    <Tab.Navigator
+      lazy={true}
+      tabBarOptions={{
+        scrollEnabled: true,
+        activeTintColor: "#FFF",
+        inactiveTintColor: "#657786",
+        indicatorStyle: { backgroundColor: "#FFF", borderWidth: 0 },
+
+        tabStyle: {
+          width: "auto",
+        },
+        style: { backgroundColor: "#282c34" },
+        labelStyle: {
+          fontWeight: "700",
+        },
+      }}
+    >
+      <Tab.Screen name="Public Groups" component={PublicGroups} />
+      {/* <Tab.Screen name="Options Alerts" component={OptionsAlerts} /> */}
+      <Tab.Screen name="Private Groups" component={PrivateGroups} />
+      <Tab.Screen name="DMs" component={PublicGroups} />
+    </Tab.Navigator>
+  );
+}
+
+export default ChatTabs;
